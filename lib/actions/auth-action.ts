d36968681be8c68a2bde87ec403c98325c66317f
Kephas7 <diyalisimon7@gmail.com>
2026-01-20 16:58:52 +0545

    AuthAction done for login and register

---
+++ lib/actions/auth-action.ts
@@ -0,0 +1,45 @@
+"use server";
+import { login, register } from "@/lib/api/auth";
+import { LoginData, RegisterData } from "@/app/(auth)/schema";
+import { success } from "zod";
+
+export const handleRegister = async (data: RegisterData) => {
+  try {
+    const response = await register(data);
+    if (response.success) {
+      return {
+        success: true,
+        message: "Registration successful",
+        data: response.data,
+      };
+    }
+    return {
+      success: false,
+      message: response.message || "Registration failed",
+    };
+  } catch (error: Error | any) {
+    return {
+      success: false,
+      message: error.message || "Registration action failed",
+    };
+  }
+};
+
+export const handleLogin = async (data: LoginData) => {
+  try {
+    const response = await login(data);
+    if (response.success) {
+      return {
+        success: true,
+        message: "Login successful",
+        data: response.data,
+      };
+    }
+    return {
+      success: false,
+      message: response.message || "Login failed",
+    };
+  } catch (error: Error | any) {
+    return { success: false, message: error.message || "Login action failed" };
+  }
+};
