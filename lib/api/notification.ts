7fa28b5324d470002b79a1051aaa3af755d62517
Kephas7 <diyalisimon7@gmail.com>
2026-02-27 23:27:57 +0545

    Notification API client added

---
+++ lib/api/notification.ts
@@ -0,0 +1,35 @@
+import axiosInstance from "./axios";
+import { API } from "./endpoints";
+
+export interface Notification {
+    _id: string;
+    userId: string;
+    type: "new_message" | "new_application" | "application_accepted" | "application_rejected" | "gig_update" | "system";
+    title: string;
+    content: string;
+    relatedId?: string;
+    isRead: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export const getNotifications = async (token: string) => {
+    const response = await axiosInstance.get(API.NOTIFICATION.GET_ALL, {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+};
+
+export const markNotificationRead = async (token: string, id: string) => {
+    const response = await axiosInstance.put(API.NOTIFICATION.MARK_READ(id), {}, {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+};
+
+export const markAllNotificationsRead = async (token: string) => {
+    const response = await axiosInstance.put(API.NOTIFICATION.MARK_READ("all"), {}, {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+};
