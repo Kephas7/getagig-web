3f69f544aca143259c4ca80a908b65af194a628e
Kephas7 <diyalisimon7@gmail.com>
2026-03-04 15:29:10 +0545

    Updated page.tsx

--- app/admin/users/create/page.tsx
+++ app/admin/users/create/page.tsx
@@ -4,7 +4,7 @@ import { getAuthToken } from "@/lib/cookies";
 export default async function CreateUserPage() {
   const token = await getAuthToken();
   return (
-    <div className="p-6">
+    <div>
       <UserForm token={token || undefined} />
     </div>
   );
